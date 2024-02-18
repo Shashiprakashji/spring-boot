3231b2084f3adeb09281c9e992c11303f3bcbaa9
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-02-18 22:39:32 +0530

    commit done

--- src/main/java/com/shashi/springboot/demo/myapp/rest/FunRestController.java
+++ src/main/java/com/shashi/springboot/demo/myapp/rest/FunRestController.java
@@ -1,10 +1,23 @@
 package com.shashi.springboot.demo.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+//    inject property for coach.name and team.name
+    @Value("${coach.name}")
+        private String coachName;
+    @Value("${team.name}")
+        private String teamName;
+
+    //now use the endpoints for teaminfo
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: "+coachName+" Team: "+teamName;
+    }
 
     @GetMapping("/")
     public String sayHello(){
