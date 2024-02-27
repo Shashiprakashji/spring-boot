ed834a2c96ae2312690bea67ab76cc61ee49c950
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-02-28 02:31:02 +0530

    commit done😂

--- src/main/java/com/shashi/springboot/controller/HelloController.java
+++ src/main/java/com/shashi/springboot/controller/HelloController.java
@@ -1,4 +1,17 @@
 package com.shashi.springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    //map the request
+//    @RequestMapping(value="/",method= RequestMethod.GET) //or we can write
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello world!";
+    }
 }
