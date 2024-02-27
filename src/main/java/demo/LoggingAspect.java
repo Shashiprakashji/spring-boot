531b896358e6a431cf42f53f6811f86786fbadcd
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-02-27 16:31:59 +0530

    commit done

--- src/main/java/demo/LoggingAspect.java
+++ src/main/java/demo/LoggingAspect.java
@@ -1,16 +1,22 @@
 package demo;
 
+
 import org.aspectj.lang.annotation.Aspect;
 
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.After;
 
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution(public void demo.ShoppingCart.checkout())")
-    public void logCheckout() {
+    @Before("execution(* demo.ShoppingCart.checkout(..))")
+    public void beforeCheckout() {
         System.out.println("Logging before checkout process.");
+    }
+    @After("execution(* *.*.checkout(..))")
+    public void afterCheckout(){
+        System.out.println("Logging after checkout process");
     }
 }
--- src/main/java/demo/ShoppingCart.java
+++ src/main/java/demo/ShoppingCart.java
@@ -8,7 +8,7 @@ public class ShoppingCart {
     //logging
     //authentication & authorization
     //sanitize data
-    public void checkout(){
+    public void checkout(String status){
         System.out.println("checkout method from ShoppingCart Class");
     }
 }
