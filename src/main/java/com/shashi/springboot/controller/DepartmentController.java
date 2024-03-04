6e64d3fd8fca7e6f93a54b30067f8728ac5e565c
Shashi Prakash <shashiprakash.bes@gmail.com>
2024-03-04 22:43:15 +0530

    commit done

--- src/main/java/com/shashi/springboot/controller/DepartmentController.java
+++ src/main/java/com/shashi/springboot/controller/DepartmentController.java
@@ -34,4 +34,9 @@ public List<Department> fetchDepartmentList(){
         departmentService.deleteDepartmentById(departmentId);
         return "Department Deleted Successfully";
 }
+
+@PutMapping("/departments/{id}")
+public Department updateDepartment(@PathVariable("id") Long departmentId,@RequestBody Department department){
+        return departmentService.updateDepartment(departmentId,department);
+}
 }
--- src/main/java/com/shashi/springboot/service/DepartmentService.java
+++ src/main/java/com/shashi/springboot/service/DepartmentService.java
@@ -16,4 +16,6 @@ public interface DepartmentService {
    public Department fetchDepartmentById(Long departmentId);
 
   public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
 }
